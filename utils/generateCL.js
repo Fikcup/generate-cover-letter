48efbf2dc59ba1ca38052d3460b6bd16afdc44a6
Fikcup <rhys.n.wright@gmail.com>
2021-10-16 17:38:18 -0700

    feat : added a today's date tool

--- utils/generateCL.js
+++ utils/generateCL.js
@@ -1,8 +1,13 @@
+const moment = require('moment');
+
 const generate = (responseData) => {
-    return `${responseData.yourname}
+    return `
+    ${responseData.yourname}
     ${responseData.yourzipcode}
     ${responseData.yournumber}
     ${responseData.youremail}
+
+    ${moment().format('MMMM Do[,] YYYY')}
     `;
 }
 
