a144135a4cb955e6ef184aa44b5c76b6be61e973
Fikcup <rhys.n.wright@gmail.com>
2021-10-16 17:41:58 -0700

    feat : added validate

--- index.js
+++ index.js
@@ -8,31 +8,61 @@ const questions = [
         type: 'input',
         name: 'yourname',
         message: 'What is your full name?',
+        validate: (input) => {
+            if (input === '') {
+                return('Please include your name.')
+            }
+        }
     },
     {
         type: 'input',
         name: 'yourzipcode',
         message: 'What is your zipcode?',
+        validate: (input) => {
+            if (input === '') {
+                return('Please include your zipcode.')
+            }
+        }
     },
     {
         type: 'input',
         name: 'yournumber',
         message: 'What is your phone number?',
+        validate: (input) => {
+            if (input === '') {
+                return('Please include your phone number.')
+            }
+        }
     },
     {
         type: 'input',
         name: 'youremail',
         message: 'What is your email?',
+        validate: (input) => {
+            if (input === '') {
+                return('Please include your email.')
+            }
+        }
     },
     {
         type: 'input',
         name: 'jobtitle',
         message: 'What is the job title?',
+        validate: (input) => {
+            if (input === '') {
+                return('Please include the job title.')
+            }
+        }
     },
     {
         type: 'input',
         name: 'companyname',
         message: 'What is the company\'s\ name?',
+        validate: (input) => {
+            if (input === '') {
+                return('Please include the company name.')
+            }
+        }
     },
 ];
 
